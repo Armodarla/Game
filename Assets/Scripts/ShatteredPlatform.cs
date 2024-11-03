e5b2f9ca53581299069c9d95c21e0c8772fc79b4
AriqHakim <ariq.hakim2002@gmail.com>
2024-11-03 15:52:33 +0700

    add respawn mechanic to shattered platform

--- Assets/Scripts/ShatteredPlatform.cs
+++ Assets/Scripts/ShatteredPlatform.cs
@@ -5,9 +5,15 @@ using UnityEngine.Tilemaps;
 public class ShatteredPlatforms : MonoBehaviour
 {
     private float fallDelay = 0.3f;
-    private float destroyDelay = 0.4f;
+    private float respDelay = 4f;
+    private Vector2 defPos;
 
     [SerializeField] private Rigidbody2D rb;
+
+    private void Start()
+    {
+        defPos = transform.position;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -25,12 +31,22 @@ public class ShatteredPlatforms : MonoBehaviour
                 StartCoroutine(Fall());
             }
         }
+        StartCoroutine(RespawnPlatform());
     }
 
     private IEnumerator Fall()
     {
         yield return new WaitForSeconds(fallDelay);
         rb.bodyType = RigidbodyType2D.Dynamic;
-        Destroy(gameObject, destroyDelay);
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        rb.AddForce(new Vector3(0, -1.0f, 0) * rb.mass * 100);
+    }
+
+    private IEnumerator RespawnPlatform()
+    {
+        yield return new WaitForSeconds(respDelay);
+        rb.bodyType = RigidbodyType2D.Static;
+        gameObject.GetComponent<Collider2D>().enabled = true;
+        transform.position = defPos;
     }
 }
